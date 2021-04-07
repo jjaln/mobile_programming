9a99600800ca24b974e61bcbbd8aceaa059c3f5d
CyberFramework <mhcho98@gmail.com>
2021-04-07 22:24:10 +0900

    add writing post in firebase

---
+++ app/src/main/java/com/jjaln/dailychart/ui/dashboard/Post.java
@@ -0,0 +1,44 @@
+package com.jjaln.dailychart.ui.dashboard;
+
+
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@IgnoreExtraProperties
+public class Post {
+
+    public String author;
+    public String password;
+    public String title;
+    public String body;
+    public int starCount = 0;
+    public Map<String, Boolean> stars = new HashMap<>();
+
+    public Post() {
+        // Default constructor required for calls to DataSnapshot.getValue(Post.class)
+    }
+
+    public Post(String author, String password, String title, String body) {
+        this.author = author;
+        this.password = password;
+        this.title = title;
+        this.body = body;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("author", author);
+        result.put("password", password);
+        result.put("title", title);
+        result.put("body", body);
+        result.put("starCount", starCount);
+        result.put("stars", stars);
+
+        return result;
+    }
+
+}

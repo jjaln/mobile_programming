96d9861c2182df50f8b1ac4b72caf2b3abd37a17
limsehyuk <ish0430@naver.com>
2021-04-10 17:31:54 +0900

    update animation

--- app/src/main/java/com/jjaln/dailychart/Setting.java
+++ app/src/main/java/com/jjaln/dailychart/Setting.java
@@ -1,4 +1,28 @@
 package com.jjaln.dailychart;
 
-public class Setting {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Setting extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        overridePendingTransition(R.anim.horizon_enter,R.anim.none);
+        getSupportActionBar().setTitle("설정");
+    }
+
+    @Override
+    public void overridePendingTransition(int enterAnim, int exitAnim) {
+        super.overridePendingTransition(enterAnim, exitAnim);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.none,R.anim.horizon_exit);
+    }
 }

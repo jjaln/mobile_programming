43544bac990803ef9bd49fe36b677505e306fd26
CyberFramework <mhcho98@gmail.com>
2021-05-01 16:06:49 +0900

    post detail and comment

--- app/src/main/java/com/jjaln/dailychart/ui/dashboard/Comment.java
+++ app/src/main/java/com/jjaln/dailychart/ui/dashboard/Comment.java
@@ -5,17 +5,17 @@ import com.google.firebase.database.IgnoreExtraProperties;
 @IgnoreExtraProperties
 public class Comment {
 
-    public String uid;
     public String author;
+    public String password;
     public String text;
 
     public Comment() {
         // Default constructor required for calls to DataSnapshot.getValue(Comment.class)
     }
 
-    public Comment(String uid, String author, String text) {
-        this.uid = uid;
+    public Comment(String author, String password, String text) {
         this.author = author;
+        this.password = password;
         this.text = text;
     }
 
---
+++ app/src/main/java/com/jjaln/dailychart/ui/dashboard/PostDetailFragment.java
@@ -0,0 +1,304 @@
+package com.jjaln.dailychart.ui.dashboard;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.jjaln.dailychart.R;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostDetailFragment extends Fragment {
+    private String[] boardList = DashboardFragment.boardList;
+    private int boardNum;
+
+    private static final String TAG = "PostDetailFragment";
+
+    public static final String EXTRA_POST_KEY = "post_key";
+
+    private DatabaseReference mPostReference;
+    private DatabaseReference mCommentsReference;
+    private ValueEventListener mPostListener;
+    private String mPostKey;
+    private CommentAdapter mAdapter;
+    private RecyclerView mRecycler;
+
+    private TextView author, title, body;
+    private Button postBtn;
+
+    private EditText commentText;
+    private EditText commentName;
+    private EditText commentPassword;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View root = inflater.inflate(R.layout.fragment_post_detail, container, false);
+
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            boardNum = bundle.getInt("boardPosition");
+        }
+
+        // This callback will only be called when MyFragment is at least Started.
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                Bundle arg = new Bundle();
+                arg.putInt("boardPosition", boardNum);
+                AppCompatActivity activity = (AppCompatActivity)getContext();
+                Fragment myFragment = new DashboardFragment();
+                myFragment.setArguments(arg);
+                activity.getSupportFragmentManager().beginTransaction().replace(R.id.action_container, myFragment).addToBackStack(null).commit();
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(getActivity(), callback);
+        // The callback can be enabled or disabled here or in handleOnBackPressed()
+
+        mRecycler = root.findViewById(R.id.recyclerPostComments);
+
+        author = (TextView)root.findViewById(R.id.postAuthor);
+        title = (TextView)root.findViewById(R.id.postTitleLayout);
+        body = (TextView)root.findViewById(R.id.postTextLayout);
+
+        postBtn = (Button)root.findViewById(R.id.buttonPostComment);
+        commentText = (EditText)root.findViewById(R.id.fieldCommentText);
+        commentName = (EditText)root.findViewById(R.id.fieldCommentName);
+        commentPassword = (EditText)root.findViewById(R.id.fieldCommentPassword);
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Get post key from arguments
+        mPostKey = requireArguments().getString(EXTRA_POST_KEY);
+        if (mPostKey == null) {
+            throw new IllegalArgumentException("Must pass EXTRA_POST_KEY");
+        }
+
+        // Initialize Database
+        mPostReference = FirebaseDatabase.getInstance().getReference()
+                .child("posts/" + boardList[boardNum]).child(mPostKey);
+        mCommentsReference = FirebaseDatabase.getInstance().getReference()
+                .child("post-comments/" + boardList[boardNum]).child(mPostKey);
+
+
+        postBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                postComment();
+            }
+        });
+        mRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Add value event listener to the post
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Post post = dataSnapshot.getValue(Post.class);
+                author.setText(post.author);
+                title.setText(post.title);
+                body.setText(post.body);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                Toast.makeText(getContext(), "Failed to load post.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+        mPostReference.addValueEventListener(postListener);
+
+        // Keep copy of post listener so we can remove it when app stops
+        mPostListener = postListener;
+
+        // Listen for comments
+        mAdapter = new CommentAdapter(getContext(), mCommentsReference);
+        mRecycler.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // Remove post value event listener
+        if (mPostListener != null) {
+            mPostReference.removeEventListener(mPostListener);
+        }
+
+        // Clean up comments listener
+        mAdapter.cleanupListener();
+    }
+
+    private void postComment() {
+        // Create new comment object
+        Comment comment = new Comment(commentName.getText().toString(), commentPassword.getText().toString(), commentText.getText().toString());
+
+        // Push the comment, it will appear in the list
+        mCommentsReference.push().setValue(comment);
+
+        // Clear the field
+        commentText.setText(null);
+    }
+
+    private static class CommentAdapter extends RecyclerView.Adapter<CommentViewHolder> {
+
+        private Context mContext;
+        private DatabaseReference mDatabaseReference;
+        private ChildEventListener mChildEventListener;
+
+        private List<String> mCommentIds = new ArrayList<>();
+        private List<Comment> mComments = new ArrayList<>();
+
+        public CommentAdapter(final Context context, DatabaseReference ref) {
+            mContext = context;
+            mDatabaseReference = ref;
+
+            // Create child event listener
+            ChildEventListener childEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                    Log.d(TAG, "onChildAdded:" + dataSnapshot.getKey());
+
+                    // A new comment has been added, add it to the displayed list
+                    Comment comment = dataSnapshot.getValue(Comment.class);
+
+                    // Update RecyclerView
+                    mCommentIds.add(dataSnapshot.getKey());
+                    mComments.add(comment);
+                    notifyItemInserted(mComments.size() - 1);
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                    Log.d(TAG, "onChildChanged:" + dataSnapshot.getKey());
+
+                    // A comment has changed, use the key to determine if we are displaying this
+                    // comment and if so displayed the changed comment.
+                    Comment newComment = dataSnapshot.getValue(Comment.class);
+                    String commentKey = dataSnapshot.getKey();
+
+                    int commentIndex = mCommentIds.indexOf(commentKey);
+                    if (commentIndex > -1) {
+                        // Replace with the new data
+                        mComments.set(commentIndex, newComment);
+
+                        // Update the RecyclerView
+                        notifyItemChanged(commentIndex);
+                    } else {
+                        Log.w(TAG, "onChildChanged:unknown_child:" + commentKey);
+                    }
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    Log.d(TAG, "onChildRemoved:" + dataSnapshot.getKey());
+
+                    // A comment has changed, use the key to determine if we are displaying this
+                    // comment and if so remove it.
+                    String commentKey = dataSnapshot.getKey();
+
+                    int commentIndex = mCommentIds.indexOf(commentKey);
+                    if (commentIndex > -1) {
+                        // Remove data from the list
+                        mCommentIds.remove(commentIndex);
+                        mComments.remove(commentIndex);
+
+                        // Update the RecyclerView
+                        notifyItemRemoved(commentIndex);
+                    } else {
+                        Log.w(TAG, "onChildRemoved:unknown_child:" + commentKey);
+                    }
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+                    Log.d(TAG, "onChildMoved:" + dataSnapshot.getKey());
+
+                    // A comment has changed position, use the key to determine if we are
+                    // displaying this comment and if so move it.
+                    Comment movedComment = dataSnapshot.getValue(Comment.class);
+                    String commentKey = dataSnapshot.getKey();
+
+                    // ...
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.w(TAG, "postComments:onCancelled", databaseError.toException());
+                    Toast.makeText(mContext, "Failed to load comments.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            };
+            ref.addChildEventListener(childEventListener);
+
+            // Store reference to listener so it can be removed on app stop
+            mChildEventListener = childEventListener;
+        }
+
+        @Override
+        public CommentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            View view = inflater.inflate(R.layout.item_comment, parent, false);
+            return new CommentViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(CommentViewHolder holder, int position) {
+            Comment comment = mComments.get(position);
+            holder.authorView.setText(comment.author);
+            holder.bodyView.setText(comment.text);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mComments.size();
+        }
+
+        public void cleanupListener() {
+            if (mChildEventListener != null) {
+                mDatabaseReference.removeEventListener(mChildEventListener);
+            }
+        }
+
+    }
+}
+
